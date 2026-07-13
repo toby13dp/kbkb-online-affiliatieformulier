d2248506dd133e864d34dfe63a43a6e6e18d6bb1
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-13 22:28:10 +0200

    Laad directe PDF-generator

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 
 document.write(
   '<script src="assets/js/form.js"><\/script>' +
+  '<script src="assets/js/pdf-core.js"><\/script>' +
   '<script src="assets/js/pdf.js"><\/script>' +
   '<script src="assets/js/main.js"><\/script>'
 );
