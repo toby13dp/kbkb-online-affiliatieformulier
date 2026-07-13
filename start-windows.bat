612040d34ad0421807b53b21640f1dae9f93945c
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:26:53 +0200

    Start Windows met gevalideerde Excelpatcher

--- start-windows.bat
+++ start-windows.bat
@@ -5,11 +5,11 @@ title KBKB Online Affiliatieformulier
 
 where py >nul 2>nul
 if %errorlevel%==0 (
-  py -3 local_server.py
+  py -3 run_local.py
 ) else (
   where python >nul 2>nul
   if %errorlevel%==0 (
-    python local_server.py
+    python run_local.py
   ) else (
     echo Python 3 is niet gevonden.
     echo Installeer Python 3 en vink tijdens de installatie "Add Python to PATH" aan.
