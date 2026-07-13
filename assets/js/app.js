f8e03bc9f43263c52dd93c0af53a6ecc57357161
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-13 22:22:56 +0200

    Schakel applicatie over naar PDF-export

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,6 @@
 
 document.write(
   '<script src="assets/js/form.js"><\/script>' +
-  '<script src="assets/js/xlsx.js"><\/script>' +
+  '<script src="assets/js/pdf.js"><\/script>' +
   '<script src="assets/js/main.js"><\/script>'
 );
