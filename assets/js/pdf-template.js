b393d7884e1223b0e6431fb8545820fbec4790ca
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:49:40 +0200

    Laad stapsgewijze ondertekeningsstijl

--- assets/js/pdf-template.js
+++ assets/js/pdf-template.js
@@ -2,4 +2,7 @@
 
 // De officiële PDF wordt tijdens de export door de lokale server opgebouwd uit
 // een tijdelijke ingevulde kopie van het originele Excelbestand.
-document.write('<link rel="stylesheet" href="assets/css/signature.css">');
+document.write(
+  '<link rel="stylesheet" href="assets/css/signature.css">' +
+  '<link rel="stylesheet" href="assets/css/signature-wizard.css">'
+);
