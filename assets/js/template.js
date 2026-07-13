c458fb906330b155afe7170b546503375575472a
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-13 22:05:01 +0200

    Koppel opgesplitst Excel-sjabloon

---
+++ assets/js/template.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.write(
+  '<script src="assets/js/template-data-1.js"><\/script>' +
+  '<script src="assets/js/template-data-2.js"><\/script>' +
+  '<script src="assets/js/template-data-3.js"><\/script>' +
+  '<script src="assets/js/template-data-4.js"><\/script>'
+);
