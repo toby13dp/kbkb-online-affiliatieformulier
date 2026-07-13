638d80a861a2498f0a90156a1182daa40c2ea636
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-13 22:05:12 +0200

    Koppel applicatiescripts

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.write(
+  '<script src="assets/js/form.js"><\/script>' +
+  '<script src="assets/js/xlsx.js"><\/script>' +
+  '<script src="assets/js/main.js"><\/script>'
+);
