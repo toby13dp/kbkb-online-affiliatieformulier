f5e7f0d706d97ecca26fe9a63c9a870e69e1bab1
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:16:51 +0200

    Laad lokale Excel-export en digitale ondertekening

---
+++ assets/js/pdf-exact.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.write(
+  '<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@04f46c6a0708418cb7b96fc563eacae0fbf77674/qrcode.min.js"><\/script>' +
+  '<script src="assets/js/signature-pad.js"><\/script>' +
+  '<script src="assets/js/signature-crypto.js"><\/script>' +
+  '<script src="assets/js/signature-flow.js"><\/script>' +
+  '<script src="assets/js/pdf-signature.js"><\/script>' +
+  '<script src="assets/js/pdf-export.js"><\/script>'
+);
