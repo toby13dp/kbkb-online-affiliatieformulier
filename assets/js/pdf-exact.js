886be63cd49b1042fedf2882e28a7d34b428aa21
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:49:53 +0200

    Laad stapsgewijze tekenwizard in het formulier

--- assets/js/pdf-exact.js
+++ assets/js/pdf-exact.js
@@ -3,8 +3,10 @@
 document.write(
   '<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@master/qrcode.min.js"><\/script>' +
   '<script src="assets/js/signature-pad.js"><\/script>' +
+  '<script src="assets/js/signature-pad-registry.js"><\/script>' +
   '<script src="assets/js/signature-crypto.js"><\/script>' +
   '<script src="assets/js/signature-flow.js"><\/script>' +
+  '<script src="assets/js/signature-wizard.js"><\/script>' +
   '<script src="assets/js/signing-copy.js"><\/script>' +
   '<script src="assets/js/pdf-signature.js"><\/script>' +
   '<script src="assets/js/pdf-export.js"><\/script>'
