241b2c0ee6c452bbf20adca4ea7db5ded92e644e
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:17:35 +0200

    Laad ondertekeningsstijl in het hoofdformulier

--- assets/js/pdf-template.js
+++ assets/js/pdf-template.js
@@ -2,3 +2,4 @@
 
 // De officiële PDF wordt tijdens de export door de lokale server opgebouwd uit
 // een tijdelijke ingevulde kopie van het originele Excelbestand.
+document.write('<link rel="stylesheet" href="assets/css/signature.css">');
