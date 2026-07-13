42685922a2937805ae47a5f68208caec7d1f13d8
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:20:02 +0200

    Laad dynamische ondertekeningsmeldingen

--- assets/js/pdf-exact.js
+++ assets/js/pdf-exact.js
@@ -5,6 +5,7 @@ document.write(
   '<script src="assets/js/signature-pad.js"><\/script>' +
   '<script src="assets/js/signature-crypto.js"><\/script>' +
   '<script src="assets/js/signature-flow.js"><\/script>' +
+  '<script src="assets/js/signing-copy.js"><\/script>' +
   '<script src="assets/js/pdf-signature.js"><\/script>' +
   '<script src="assets/js/pdf-export.js"><\/script>'
 );
