bc8dc2b61088b0a751d4fbf087fb6764553d2d03
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 00:18:51 +0200

    Gebruik beschikbare QR-codebibliotheek

--- assets/js/pdf-exact.js
+++ assets/js/pdf-exact.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.write(
-  '<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@04f46c6a0708418cb7b96fc563eacae0fbf77674/qrcode.min.js"><\/script>' +
+  '<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@master/qrcode.min.js"><\/script>' +
   '<script src="assets/js/signature-pad.js"><\/script>' +
   '<script src="assets/js/signature-crypto.js"><\/script>' +
   '<script src="assets/js/signature-flow.js"><\/script>' +
