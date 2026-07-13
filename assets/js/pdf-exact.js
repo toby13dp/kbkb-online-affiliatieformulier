798677d1f093fefd0f112e42f40888ccb6aae53b
Toby De Prins <141472421+toby13dp@users.noreply.github.com>
2026-07-14 01:58:26 +0200

    Verwijder ongebruikte globale zoommodule

--- assets/js/pdf-exact.js
+++ assets/js/pdf-exact.js
@@ -5,7 +5,6 @@ document.write(
   '<script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@master/qrcode.min.js"><\/script>' +
   '<script src="assets/js/signature-pad.js"><\/script>' +
   '<script src="assets/js/signature-pad-registry.js"><\/script>' +
-  '<script src="assets/js/signature-zoom.js"><\/script>' +
   '<script src="assets/js/signature-crypto.js"><\/script>' +
   '<script src="assets/js/signature-flow.js"><\/script>' +
   '<script src="assets/js/signature-wizard.js"><\/script>' +
